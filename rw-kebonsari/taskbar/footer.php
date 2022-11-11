5914ddce4e69dfae7ee4131bacc3ccf476c77cb7
satriafauji <102895019+satriafauji@users.noreply.github.com>
2022-11-11 18:31:43 +0700

    project KP

---
+++ rw-kebonsari/taskbar/footer.php
@@ -0,0 +1,121 @@
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+  
+  <!-- Footer -->
+    <footer class="text-center text-lg-start bg-white text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="kantor"></i>KANTOR RW 06 KEBONSARI
+          </h6>
+          <p>
+            Kebonsari memiliki semangat juang dalam hal kesehatan dengan membangun fasilitas GOR
+            yang bisa digunakan dalam beberapa kegiatan masyrakat seperti Pingpong, Pencaksilat, Marawis dan kegiatan lainnya
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Prestasi
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Lomba Kebersihan</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Lomba Tenis Meja</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Lomba administrasi</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Lomba </a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Hubungi Kami</h6>
+          <p><i class="alamat"></i>Jln. Kebonsari No. Rt 03 Rw 06. Baros, Cimahi, 40521</p>
+          <p><i class="email"></i>satriafauji5@gmail.com</p>
+          <p><i class="telp"></i>No.Telp: 123456789</p>
+          <p><i class="wa"></i>Whatsapp: 123456789</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">GetBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
